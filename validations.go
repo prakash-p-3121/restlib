ae02c01af6a152964d4fcbef502333c8eb4ed00a
Prakash Premkumar <prakash.p.2292@gmail.com>
2024-03-15 11:51:43 +0530

    trim and check for empty string

--- validations.go
+++ validations.go
@@ -1,5 +1,17 @@
 package restlib
 
+import (
+	"strings"
+)
+
 func IsEmptyString(value string) bool {
 	return len(value) == 0
+}
+
+func TrimAndCheckForEmptyString(value *string) bool {
+	if value == nil {
+		return false
+	}
+	*value = strings.TrimSpace(*value)
+	return len(*value) == 0
 }
